b77a90b334beddc9a86600e99b0c02c64be31846
Leo-Leejianzhao <leejianzhao@gmail.com>
2021-11-25 23:05:47 +0800

    OK

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 '''
 Author: Leo Lee (leejianzhao@gmail.com)
 Date: 2021-07-18 16:34:45
-LastEditTime: 2021-11-25 23:00:00
+LastEditTime: 2021-11-25 23:04:47
 FilePath: \RSS\main.py
 Description:
 '''
@@ -110,7 +110,7 @@ def IP2name(ip):
         res=requests.get(f'http://ip-api.com/json/{ip}?fields=country,countryCode,city&lang=zh-CN', timeout=10).json()
         return f"{ip}@{res['country']}({res['countryCode']})-{res['city']}/"+''.join(random.sample(string.ascii_letters + string.digits, 3))
     except Exception as e:
-        log('IP2name: '+ip+': '+e.__str__()+'\nres='+res)
+        log('IP2name: '+ip+': '+e.__str__())
         return ip+''.join(random.sample(string.ascii_letters + string.digits, 8))
 
 # https://github.com/p4gefau1t/trojan-go/issues/132
