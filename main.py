3c3e18fce62a5cad6265eb64b6b93630f9ac3df7
Leo-Leejianzhao <87597944+Leo-Leejianzhao@users.noreply.github.com>
2022-09-30 10:20:03 +0800

    Update main.py

--- main.py
+++ main.py
@@ -290,7 +290,7 @@ if __name__ == '__main__':
     
     proxies.extend(load_subscribe_url('https://shadowshare.v2cross.com/publicserver/servers/temp/ud4HOnWAsQxBmSIl'))
     
-    proxies.extend(load_subscribe_url('https://shadowshare.v2cross.com/servers/temp/KCasomEg9pTUwbjL'))
+    proxies.extend(load_subscribe_url('https://shadowshare.v2cross.com/servers/temp/Zy0I6U427wmSsvnJ'))
     
     # proxies.extend(load_subscribe_url('https://fforever.github.io/v2rayfree'))
     # proxies.extend(load_subscribe_url('https://muma16fx.netlify.app'))
