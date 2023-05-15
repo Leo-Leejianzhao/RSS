48d20dd7b331711293a478ab8915f466ec5f7c4e
Leo-Leejianzhao <87597944+Leo-Leejianzhao@users.noreply.github.com>
2023-05-15 22:24:16 +0800

    Update main.py

--- main.py
+++ main.py
@@ -290,7 +290,7 @@ if __name__ == '__main__':
     
     proxies.extend(load_subscribe_url('https://shadowshare.v2cross.com/publicserver/servers/temp/ud4HOnWAsQxBmSIl'))
     
-    proxies.extend(load_subscribe_url('https://sub.id9.cc/sub?target=v2ray&url=https://9527521.xyz/config/PekNaJ4oHYjT7Lpr&insert=false'))
+    proxies.extend(load_subscribe_url('https://sub.id9.cc/sub?target=v2ray&url=https://9527521.xyz/config/rFlJUad7X2smVieZ&insert=false'))
     
     # proxies.extend(load_subscribe_url('https://fforever.github.io/v2rayfree'))
     # proxies.extend(load_subscribe_url('https://muma16fx.netlify.app'))
