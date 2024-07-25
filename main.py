f17b6cd040124322587e543a77fe564c4302afab
Leo-Leejianzhao <87597944+Leo-Leejianzhao@users.noreply.github.com>
2024-07-26 00:09:19 +0800

    Update main.py

--- main.py
+++ main.py
@@ -208,7 +208,7 @@ def protocol_decode(proxy_str):
                     "port": port,
                     "password": password,
                     "alterId": 2,
-                    "cipher": cipher if cipher!="ss" else "auto",
+                    "cipher": cipher if cipher!="ss" else "",
                 }
         except Exception as e:
             log('Invalid vmess URL:'+proxy_str)
